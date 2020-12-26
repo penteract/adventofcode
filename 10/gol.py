22f1cd3d6bc219481050056bea88ecac1280f55a
Toby Cathcart Burn <tcathcartburn@gmail.com>
2020-12-26 20:34:59 +0000

    Fix bug in golfed day 10

--- 10/gol.py
+++ 10/gol.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 d=defaultdict(int,{0:1})
-for x in sorted([int(x) for x in open("input")]):
+for x in (l:=sorted([int(x) for x in open("input")])):
     d[x]=d[x-1]+d[x-2]+d[x-3]
 print(d[l[-1]])
     
