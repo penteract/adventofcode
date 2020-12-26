5fd4c77b6a0263d486ab40331f9dbf2ab185e961
Toby Cathcart Burn <tcathcartburn@gmail.com>
2020-12-26 20:41:39 +0000

    improved golfed day 10

--- 10/gol.py
+++ 10/gol.py
@@ -2,5 +2,4 @@ from collections import defaultdict
 d=defaultdict(int,{0:1})
 for x in (l:=sorted([int(x) for x in open("input")])):
     d[x]=d[x-1]+d[x-2]+d[x-3]
-print(d[l[-1]])
-    
+print(d[x])
