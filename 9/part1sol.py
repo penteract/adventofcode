276ecd0229a958f9842b5da80d4c1849c36b39f4
Toby Cathcart Burn <tcathcartburn@gmail.com>
2023-12-09 07:46:32 +0000

    Day 9

---
+++ 9/part1sol.py
@@ -0,0 +1,80 @@
+import re
+from typing import Tuple, Callable, Iterable, Optional
+import sys
+fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
+
+from collections import defaultdict
+
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+dirs = {
+    "R":Pt((0,1)),
+    "U":Pt((1,0)),
+    "L":Pt((0,-1)),
+    "D":Pt((-1,0))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line.strip() for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+
+d=defaultdict(int)
+tot=0
+
+def gets(s):
+    if all(x==0 for x in s):
+        return 0
+    else:
+        return s[-1] + gets([b-a for a,b in zip(s,s[1:])])
+
+#print(lmap(ints_locs,f))
+for line in xss:
+    tot += gets(line)
+    
+print(tot)
