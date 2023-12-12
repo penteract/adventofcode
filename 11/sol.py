32c6017980f8e7b0f36d862bbaad54c7b5972e62
Toby Cathcart Burn <tcathcartburn@gmail.com>
2023-12-12 04:56:33 +0000

    Day 11

---
+++ 11/sol.py
@@ -0,0 +1,103 @@
+import re
+from typing import Tuple, Callable, Iterable, Optional
+import sys
+fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
+
+from collections import defaultdict
+
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+dirs = {
+    "R":Pt((1,0)),
+    "U":Pt((0,-1)),
+    "L":Pt((-1,0)),
+    "D":Pt((0,1))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line.strip() for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+d=defaultdict(int)
+
+if fname =="input":
+    kk=1000000
+else:
+    kk=10
+
+d={}
+yoff = 0
+for y,r in enumerate(f):
+    k=False
+    for x,c in enumerate(r):
+        if (c=="#"):
+            d[(x,y)]= (x,y+yoff*(kk-1))
+            k=True
+    if not k:
+        yoff +=1
+
+nd=set()
+xoff=0
+for x,c in enumerate(f[0]):
+    k=False
+    for y in range(len(f)+1):
+        if (x,y) in d:
+            xx,ny = d[x,y]
+            nd.add((xx+xoff*(kk-1),ny))
+            k=True
+    if not k:
+        xoff+=1
+
+
+tot=0
+
+for x,y in nd:
+    for w,z in nd:
+        tot+=abs(x-w)+abs(z-y)
+
+print(tot//2)
