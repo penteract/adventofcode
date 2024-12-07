abfb43cd34a1464824bc67211676ec3ef4506f09
Toby Cathcart Burn <tcathcartburn@gmail.com>
2024-12-07 06:00:41 +0000

    2024 Day 7

---
+++ 7/solfast.py
@@ -0,0 +1,150 @@
+import re
+from typing import Tuple, Callable, Iterable, Optional
+import sys
+fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
+
+from collections import defaultdict
+
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+def pt(*args):
+    if len(args)==1: return Pt(args[0])
+    else: return Pt(args)
+dirs = {
+    "R":Pt((1,0)),
+    "U":Pt((0,-1)),
+    "L":Pt((-1,0)),
+    "D":Pt((0,1))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line.strip() for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+xs=[]
+ys=[]
+
+d=defaultdict(int)
+tot=0
+
+for y,line in enumerate(flns):    
+    for x,c in enumerate(line):
+        d[x,y]=c
+    
+#print(lmap(ints_locs,f))
+import math
+
+
+print(math.log(100,10))
+def makes(a,xs):
+    #print(xs)
+    if len(xs)==1:
+        return a==xs[0]
+    else:
+        k = xs[-1]
+        rs = xs[:-1]
+        if a%k==0 and makes(a//k,rs):
+            return True
+        if a>=k and makes(a-k,rs):
+            return True
+        #lenk = 10**(math.floor(math.log(k,10))+1)
+        #lenk = 10**len(str(k))
+        #if a%lenk == k and makes(a//lenk,rs) :
+        sa = str(a)
+        sk = str(k)
+        #lenk = 10**len(sk)
+        if sa.endswith(sk) and makes(int(sa+sk),rs) :
+            return True
+        return False
+        #assert acc*(10**(math.floor(math.log(k,10))+1))+k == int(str(acc)+str(k))
+        #return any(makes(a,ac,xs[1:]) for ac in [acc+k,acc*k,acc*(10**(math.floor(math.log(k,10))+1))+k ])
+#print(len(xss))
+#print(xss)
+import timeit
+print(timeit.timeit("""
+tot=0
+for x in xss:
+    p=x[0]
+    #print(x)
+    tot+=p*makes(p,x[1:])""",globals={"makes":makes,"xss":xss},number=1000))
+tot=0
+for x in xss:
+    p=x[0]
+    #print(x)
+    tot+=p*makes(p,x[1:])
+print(tot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
