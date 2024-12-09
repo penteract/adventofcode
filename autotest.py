35366d69264f008c5a9de3c3cb6a4f5797280790
Toby Cathcart Burn <tcathcartburn@gmail.com>
2024-12-09 05:42:16 +0000

    get autotest to resubmit better

--- autotest.py
+++ autotest.py
@@ -289,6 +289,13 @@ def find_examples(part):
     return [("input1",sampleout)]
 
 
+def asksub():
+    ans="?"
+    while ans not in "nN":
+        ans = input("Do you want to submit "+repr(answer)+" (y/n)?")
+        if ans in "yY":
+            return True
+    return False
 
 def doPart(part=None):
     if part is None:
@@ -350,7 +357,7 @@ def doPart(part=None):
                 print(repr(answer),
                       f"is too low; as {bad_toolow} was. Not submitting.")
             else:
-                if not bad_answers or input("Do you want to submit "+repr(answer)+" (y/n)?")=="y":
+                if not bad_answers or asksub():
                     print("submitting answer:",repr(answer))
                     resp,content = submit(part=part,answer=answer)
                     if "That's the right answer!" in content:
