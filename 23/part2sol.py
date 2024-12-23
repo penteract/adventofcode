7e1aaeb40f06f813fc02f528ef4a615c7c1d7c95
Toby Cathcart Burn <tcathcartburn@gmail.com>
2024-12-23 08:34:52 +0000

    2024 Day 23

---
+++ 23/part2sol.py
@@ -0,0 +1,143 @@
+import re
+from typing import Tuple, Callable, Iterable, Optional
+import sys
+import math,functools,itertools
+fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
+
+from collections import defaultdict
+
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+def pt(*args):
+    if len(args)==1: return Pt(args[0])
+    else: return Pt(args)
+dirs = {
+    "R":Pt((1,0)),
+    "U":Pt((0,-1)),
+    "L":Pt((-1,0)),
+    "D":Pt((0,1))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line.strip() for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+tot=0
+d=defaultdict(list)
+dd=defaultdict(set)
+
+l=[]
+for y,line in enumerate(flns):
+    a,b = line.split("-")
+    l.append((a,b))
+    d[a].append(b)
+    d[b].append(a)
+import networkx as nx
+G = nx.Graph()
+
+G.add_edges_from(l)
+mx = 0
+mc=0
+for c in nx.find_cliques(G):
+    if len(c)>mx:
+        mc=c
+        mx=len(c)
+
+print(",".join(sorted(mc)))
+
+"""
+l = list(d)
+
+r=set()
+for c in l:
+    if c.startswith("t"):
+        for i,a in enumerate(d[c]):
+            #print(c,a)
+            for j,b in enumerate(d[c][i+1:]):
+                #print(a,b,c)
+                if b in d[a]:
+                    r.add(tuple(sorted([a,b,c])))
+                    #tot+=1
+
+
+print("graph g {")
+print("}")
+
+tot=len(r)
+
+print(tot)
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
