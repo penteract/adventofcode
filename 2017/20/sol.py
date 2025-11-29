3a542c3ab7fdeb7799068fffae1f591084fe9c17
Toby Cathcart Burn <tcathcartburn@gmail.com>
2025-11-29 20:54:24 +0000

    2017 day 20

--- 2017/20/sol.py
+++ 2017/20/sol.py
@@ -1,36 +1,123 @@
-from collections import defaultdict
+import re
+from typing import Tuple, Callable, Iterable, Optional
 import sys
+import math,functools,itertools
 fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
 
-f=list(open(fname))
+from collections import defaultdict
 
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+def pt(*args):
+    if len(args)==1: return Pt(args[0])
+    else: return Pt(args)
+dirs = {
+    "R":Pt((1,0)),
+    "U":Pt((0,-1)),
+    "L":Pt((-1,0)),
+    "D":Pt((0,1))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line.strip() for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+tot=0
 d=defaultdict(int)
 
-r=[]
-for y,line in enumerate(f):
-    l=list(map(int,"".join(c if c in "1234567890-" else " " for c in line).split()))
-    r.append((l[:3],l[3:6],l[6:]))
-    #for x,c in enumerate(line.strip()):
-    #    d[(x+1j*y)]=int(c)+1
+ps = []
+for y,line in enumerate(flns):
+    ig,p,v,a = line.split("<")
+    p,v,a=[Pt(ints(x)) for x in [p,v,a]]
+    ps.append([p,v,a])
 
-print(r[-1])
+def mg(p):
+    return abs(p[0])+abs(p[1])+abs(p[2])
 
-def sign(x):
-    if x==0:
-        return 0
-    return x/abs(x)
+s = sorted(enumerate(ps),key=lambda x:mg(x[1][2]))
+#print(mg(s[0][2]),mg(s[1][2]))
+print(s[0])
+print(s[1])
 
-flag = True
-while flag:#loop until signs of each component agree
-    flag=False
-    for p,v,a in r:
-        for i in range(3):
-            v[i]+=a[i]
-            p[i]+=v[i]
-        if not all(sign(p[i])==sign(v[i])==sign(a[i]) for i in range(3)):
-            flag=True
+for i in range(100):
+    seen=defaultdict(int)
+    for p in ps:
+        p[1]+=p[2]
+        p[0]+=p[1]
+        seen[p[0]]+=1
+    ps = [x for x in ps if seen[x[0]]<=1]
 
-l = [(d,c,b,a) for a,(b,c,d) in enumerate(r)]
-l.sort()
-print (l[0])
-    
+print(len(ps))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
