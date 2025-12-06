6af103427db116f65e41563d545a6273b5a25493
Toby Cathcart Burn <tcathcartburn@gmail.com>
2025-12-06 05:56:46 +0000

    2025 Day 6

---
+++ 6/sol.py
@@ -0,0 +1,136 @@
+import re
+from typing import Tuple, Callable, Iterable, Optional
+import sys
+import math,functools,itertools
+fname=sys.argv[1] if len(sys.argv)>1 else "input"
+f = open(fname)
+ftext=f.read()
+
+from collections import defaultdict
+
+#copied from https://github.com/joefarebrother/adventofcode/blob/master/misc_utils.py
+def lmap(f: Callable, *xs) -> list:
+    return list(map(f, *xs))
+def mint(x, default=None):
+    try: return int(x)
+    except ValueError: return default
+def ints(x: str) -> list[int]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return lmap(int,re.findall(ex, x))
+
+def ints_locs(x: str) -> list[tuple[int,tuple[int,int]]]:
+    ex = r'(?:(?<!\d)-)?\d+'
+    return [(int(x.group()),x.span()) for x in re.finditer(ex, x)]
+
+class Pt(tuple):
+    def __add__(self,other):
+        return Pt([x+y for x,y in zip(self,other,strict=True)])
+    def __sub__(self,other):
+        return Pt([x-y for x,y in zip(self,other,strict=True)])
+    def __rsub__(self,other):
+        return Pt([y-x for x,y in zip(self,other,strict=True)])
+    def __radd__(self,other):
+        return Pt([y+x for x,y in zip(self,other,strict=True)])
+    def __rmul__(self,other):
+        return Pt([other*x for x in self])
+    def left(self):
+        x,y=self
+        return Pt((y,-x))
+    def right(self):
+        x,y=self
+        return Pt((-y,x))
+def pt(*args):
+    if len(args)==1: return Pt(args[0])
+    else: return Pt(args)
+dirs = {
+    "R":Pt((1,0)),
+    "U":Pt((0,-1)),
+    "L":Pt((-1,0)),
+    "D":Pt((0,1))
+    }
+ods = list(dirs.values())
+
+#odirs = [(0,1),(1,0),(0,-1),(-1,0)]
+ddirs = lmap(Pt,[(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)])
+
+##Input handling
+flns=[line for line in ftext.split("\n")[:-1]]
+f=flns
+
+fgroups = ftext.split("\n\n")
+if len(fgroups)>1:
+    fgs = [g.split("\n") for g in fgroups]
+    fgns = [lmap(ints,g) for g in fgs]
+
+try: xs = lmap(int,f)
+except Exception: pass
+try: xss = lmap(ints,f)
+except Exception: pass
+
+tot=0
+#d=defaultdict(int)
+d={}
+l = len(flns)
+ins=lmap(ints,flns[:l-1])
+#d = flns[l-1].split()
+#print(lmap(len,[a,b,c,d]))
+"""for q,w,e,g,r in zip(*(ins+[d])):
+    print(r)
+    if r=="+":
+        tot+=q+w+e+g
+    else:
+        tot+=q*w*e*g
+"""
+for i,line in enumerate(flns[:l-1]):
+    for j,c in enumerate(line):
+        d[i,j]=c
+print(flns[-1])
+v=0
+for i,c in enumerate(flns[-1]):
+    if c!=" ":
+        tot+=v
+        v=(0 if c=="+" else 1)
+        op=c
+        print()
+    try:
+        k=int("".join([d[j,i] for j in range(l-1)]))
+        print(k)
+        if op=="+":
+            v+=k
+        else:
+            v*=k
+    except Exception:
+        pass
+
+tot+=v
+print(tot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
